c592158f0b549f6ba491e837b8e53656da40c719
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2021-02-13 22:38:33 +0000

    Finish Fetching a single news item from database

--- src/main/java/com/mohamed/hadramy/springbootapp/controller/NewsController.java
+++ src/main/java/com/mohamed/hadramy/springbootapp/controller/NewsController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import com.mohamed.hadramy.springbootapp.model.News;
 import com.mohamed.hadramy.springbootapp.repository.NewsRepository;
@@ -15,6 +16,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,22 +32,25 @@ public class NewsController {
     public ResponseEntity<Map<String, Object>> getAllNewsPage(@RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "8") int size) {
 
-                try{
-                    List<News> news = new ArrayList<News>();
-                    Pageable firstPageWithEightItems = PageRequest.of(page, size);
-                    Page<News> newsPage = newsRepository.findAllByOrderByIdDesc(firstPageWithEightItems);
-                    news = newsPage.getContent();
-                    Map<String, Object> response = new HashMap<>();
-                    response.put("news", "vuy");
-                    response.put("news", news);
-                    return new ResponseEntity<>(response, HttpStatus.OK);
-                }
-                catch (Exception e) {
-                    return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-                }
+        try {
+            List<News> news = new ArrayList<News>();
+            Pageable firstPageWithEightItems = PageRequest.of(page, size);
+            Page<News> newsPage = newsRepository.findAllByOrderByIdDesc(firstPageWithEightItems);
+            news = newsPage.getContent();
+            Map<String, Object> response = new HashMap<>();
+            response.put("news", "vuy");
+            response.put("news", news);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
-
-
+    @GetMapping("/news/{id}")
+    public ResponseEntity<News> getNewsById(@PathVariable("id") Long id){
+        Optional<News> newsItem = newsRepository.findById(id);
+        if (newsItem.isPresent())  return new ResponseEntity<>(newsItem.get(), HttpStatus.OK);
+        else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 }
