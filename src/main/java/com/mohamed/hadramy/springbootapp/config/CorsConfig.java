2109d415c95452435363b373dc44ca72a681ec27
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2021-03-07 00:08:03 +0000

    Change port to 8084 and allow all origins to access api calls

--- src/main/java/com/mohamed/hadramy/springbootapp/config/CorsConfig.java
+++ src/main/java/com/mohamed/hadramy/springbootapp/config/CorsConfig.java
@@ -9,6 +9,6 @@ public class CorsConfig implements WebMvcConfigurer {
     
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*");
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
     }
 }
